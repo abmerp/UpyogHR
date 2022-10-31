e715721e89011d0fcac70bd9ff698b0d63a40fe5
palamgarg <palamgarg@gmail.com>
2022-10-31 22:43:13 +0530

    Commit

--- core-services/user-otp/src/main/java/org/egov/persistence/repository/OtpSMSRepository.java
+++ core-services/user-otp/src/main/java/org/egov/persistence/repository/OtpSMSRepository.java
@@ -61,9 +61,9 @@ public class OtpSMSRepository {
         Map<String, String> localisedMsgs = localizationService.getLocalisedMessages(tenantId, "en_IN", "egov-user");
         if (localisedMsgs.isEmpty()) {
             log.info("Localization Service didn't return any msgs so using default...");
-            localisedMsgs.put(LOCALIZATION_KEY_REGISTER_SMS, "Dear Citizen, Your OTP to complete your mSeva Registration is %s.");
+            localisedMsgs.put(LOCALIZATION_KEY_REGISTER_SMS, "Your confidential login OTP is %s.");
             localisedMsgs.put(LOCALIZATION_KEY_LOGIN_SMS, "Your confidential login OTP is %s.");
-            localisedMsgs.put(LOCALIZATION_KEY_PWD_RESET_SMS, "Dear Citizen, Your OTP for recovering password is %s.");
+            localisedMsgs.put(LOCALIZATION_KEY_PWD_RESET_SMS, "Your confidential login OTP is %s.");
         }
         String message = null;
 
