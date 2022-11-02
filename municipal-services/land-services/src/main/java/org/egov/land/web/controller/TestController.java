94b760919cfbf24cab9e3d60e95a4ec002481485
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2022-11-02 23:46:35 +0530

    changes

--- municipal-services/land-services/src/main/java/org/egov/land/web/controller/TestController.java
+++ municipal-services/land-services/src/main/java/org/egov/land/web/controller/TestController.java
@@ -24,38 +24,64 @@ public class TestController {
 	@PostMapping(value = "/getToken")
 	public ResponseEntity<Map> token(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 
-			return partyAPiCall.getAuthToken();
+		return partyAPiCall.getAuthToken();
 	}
+
 	@PostMapping(value = "/_TransactionNumber")
 	public ResponseEntity<Map> generateTransactionNo(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("MobNo", "7589489623");
+		map.put("CaseTypeId", "1");
+		map.put("AppTypeId", "1");
+		map.put("ChargesTypeId", "34");
+
 		return partyAPiCall.generateTransactionNumber(map);
 	}
-		@PostMapping(value = "/_SaveTransactionNumber")
-		public ResponseEntity<Map> TransactionData(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 
-			Map<String, Object> map = new HashMap<String, Object>();
-			return partyAPiCall.saveTransactionData(map);
-	
-}
+	@PostMapping(value = "/_SaveTransactionNumber")
+	public ResponseEntity<Map> TransactionData(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("UserName", "manoj");
+		map.put("MobNo", "7589489623");
+		map.put("CaseTypeId", "1");
+		map.put("AppTypeId", "1");
+		map.put("ChargesTypeId", "34");
+		map.put("TxnAmount", "200.23");
+		map.put("LicenceFeeNla", "150.1");
+		map.put("ScrutinyFeeNla", "50.2");
+		map.put("PaymentMode", "101");
+		map.put("PayAgreegator", "0300997");
+		map.put("LcApplicantName", "DLF Ltd");
+		map.put("LcPurpose", "TestPurpose");
+		map.put("LcDevelopmentPlan", "GN");
+		map.put("LcDistrict", "Gurugram");
+
+		return partyAPiCall.saveTransactionData(map);
+
+	}
+
 	@PostMapping(value = "/_Dairynumber")
 	public ResponseEntity<Map> DiaryNumber(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 		Map<String, Object> map = new HashMap<String, Object>();
+
 		return partyAPiCall.generateDiaryNumber(map);
-}
+	}
+
 	@PostMapping(value = "/_CaseNumber")
 	public ResponseEntity<Map> generateCaseNumber(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		return partyAPiCall.generateCaseNumber(map);
-}
+	}
+
 	@PostMapping(value = "/_ApplicationNumber")
 	public ResponseEntity<Map> generateApplicationNumber(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		return partyAPiCall.generateApplicationNumber(map);
-}
+	}
+
 	@PostMapping(value = "/_SSOToken")
 	public ResponseEntity<Map> isExistSSOToken(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		return partyAPiCall.isExistSSOToken(map);
-}
+	}
 }
