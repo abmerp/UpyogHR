eaaa46b5db53d77fc17f2d34c0250a21ae7d1f3b
palamgarg <palamgarg@gmail.com>
2022-11-14 20:02:04 +0530

    Added Developer info

---
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/models/AddRemoveAuthoizedUsers.java
@@ -0,0 +1,32 @@
+package org.egov.tl.web.models;
+
+import java.util.List;
+
+//import org.egov.user.domain.model.Role;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddRemoveAuthoizedUsers {
+
+//	private int serialNumber;
+	private String userName;
+	private String name;
+	private String gender;
+	private boolean active;
+	private String type;
+	private String password;
+	private String tenantId;
+	private String mobileNumber;
+//	private String email;
+//	private String uploadPanPdf;
+//	private String uploadAadharPdf;
+//	private String uploadDigitalSignaturePdf;
+	//private List<Role> roles;
+	private String emailId;
+	private String dob;
+	private String pan;
+
+}
---
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/models/DesignationDirector.java
@@ -0,0 +1,14 @@
+package org.egov.tl.web.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DesignationDirector {
+
+	  private String agreementDoc;
+      private String boardDoc;
+	
+	
+}
