d5ecda8cb05b551ef4e65ddb8bd1bf4378bf34c1
palamgarg <palamgarg@gmail.com>
2022-11-14 14:46:28 +0530

    Fixed null pointer

--- core-services/egov-user/src/main/java/org/egov/user/abm/developer/controller/DeveloperRegistrationController.java
+++ core-services/egov-user/src/main/java/org/egov/user/abm/developer/controller/DeveloperRegistrationController.java
@@ -46,9 +46,11 @@ public class DeveloperRegistrationController {
 	@GetMapping("/_getDeveloperById")
 	public DeveloperDetailResponse getById(@RequestParam(value = "id") Long id, boolean isAllData) {
 		DeveloperRegistration developerRegistration1 = developerRegistrationService.getById(id, isAllData);
-		List<Developerdetail> listDevelopers = developerRegistration1.getDeveloperDetail();
+		List<Developerdetail> listDevelopers =null;
+		if(developerRegistration1!=null && developerRegistration1.getDeveloperDetail().size()>0)
+		listDevelopers = developerRegistration1.getDeveloperDetail();
 		ResponseInfo responseInfo = ResponseInfo.builder().status(String.valueOf(HttpStatus.OK.value())).build();
-		DeveloperDetailResponse response;
+		DeveloperDetailResponse response=null;
 		if (isAllData) {
 			response = new DeveloperDetailResponse(responseInfo, developerRegistration1.getId(),
 					developerRegistration1.getCurrentVersion(), developerRegistration1.getCreatedBy().toString(),
@@ -56,13 +58,16 @@ public class DeveloperRegistrationController {
 					developerRegistration1.getUpdatedDate(), listDevelopers);
 		} else {
 			List<Developerdetail> listDevelopers1 = new ArrayList<Developerdetail>();
-
+			if(listDevelopers!=null) {
 			listDevelopers1.addAll(listDevelopers);
-			
-			response = new DeveloperDetailResponse(responseInfo, developerRegistration1.getId(),
+						response = new DeveloperDetailResponse(responseInfo, developerRegistration1.getId(),
 					developerRegistration1.getCurrentVersion(), developerRegistration1.getCreatedBy().toString(),
 					developerRegistration1.getCreatedDate(), developerRegistration1.getUpdateddBy().toString(),
 					developerRegistration1.getUpdatedDate(), listDevelopers1);
+			}else
+			{
+				
+			}
 		}
 		return response;
 
