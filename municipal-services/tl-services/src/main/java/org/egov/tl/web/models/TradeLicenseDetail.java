cf880c619fa87be815f62bc6bb70e6d25c6c4537
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2022-12-17 11:24:27 +0530

    yml file

--- municipal-services/tl-services/src/main/java/org/egov/tl/web/models/TradeLicenseDetail.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/models/TradeLicenseDetail.java
@@ -106,6 +106,9 @@ public class TradeLicenseDetail   {
 
         @JsonProperty("additionalDetail")
         private JsonNode additionalDetail = null;
+        
+        @JsonProperty("currentVersion")
+        private float currentVersion;
 
     
 
