bb8df52463ea9674111e67da3cb34926bd69f44c
Renu <110284883+Renu-1998@users.noreply.github.com>
2023-01-09 11:49:07 +0530

    applicantInfo

--- municipal-services/tl-services/src/main/java/org/egov/tl/web/models/ApplicantInfo.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/models/ApplicantInfo.java
@@ -8,7 +8,46 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ApplicantInfo {
-
+	@JsonProperty("devDetail")
+	private Developerdetail devDetail;
+	@JsonProperty("authorizedDeveloper")
+	private String authorizedDeveloper;
+	@JsonProperty("authorizedPerson")
+	private String authorizedPerson;
+	@JsonProperty("authorizedmobile")
+	private String authorizedmobile;
+	@JsonProperty("alternatemobile")
+	private String alternatemobile;
+	@JsonProperty("authorizedEmail")
+	private String authorizedEmail;
+	@JsonProperty("authorizedPan")
+	private String authorizedPan;
+	@JsonProperty("authorizedAddress")
+	private String authorizedAddress;
+	@JsonProperty("village")
+	private String village;
+	@JsonProperty("authorizedPinCode")
+	private Integer authorizedPinCode;
+	@JsonProperty("tehsil")
+	private String tehsil;
+	@JsonProperty("district")
+	private String district;
+	@JsonProperty("state")
+	private String state;
+	@JsonProperty("status")
+	private String status;
+	@JsonProperty("")
+	private String LC;
+	@JsonProperty("address")
+	private String address;
+	@JsonProperty("permanentAddress")
+	private String permanentAddress;
+	@JsonProperty("notSigned")
+	private String notSigned;
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("authorized")
+	private String authorized;
 	@JsonProperty("developerName")
 	private String developerName;
 	@JsonProperty("developerAddress")
@@ -39,11 +78,6 @@ public class ApplicantInfo {
 	private String authorizedName;
 	@JsonProperty("authorizedMobile")
 	private String authorizedMobile;
-	@JsonProperty("authorizedEmail")
-	private String authorizedEmail;
-	@JsonProperty("authorizedPan")
-	private String authorizedPan;
-	@JsonProperty("authorizedAddress")
-	private String authorizedAddress;
+
 
 }
