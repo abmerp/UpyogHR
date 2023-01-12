3bf996cf42b7234562151a9d2f0537e7a0eb0db1
palamgarg <palamgarg@gmail.com>
2023-01-12 18:55:23 +0530

    data

--- core-services/egov-user/src/main/java/org/egov/user/domain/service/UserService.java
+++ core-services/egov-user/src/main/java/org/egov/user/domain/service/UserService.java
@@ -694,7 +694,7 @@ public class UserService {
 		ssoCitizenMap.put("TokenId", ssoCitizen.getTokenId());
 		ResponseEntity<Map> isExistSSOToken = null;
 		String ssoValue = "no";
-
+		log.info("Data by Request"+ssoCitizenMap);
 		try {
 			isExistSSOToken = isExistSSOToken(ssoCitizenMap);
 
