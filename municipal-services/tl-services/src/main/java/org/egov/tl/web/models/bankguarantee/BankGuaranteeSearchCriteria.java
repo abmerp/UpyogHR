36f371c61876f581aa7ffe63ad1e77fdf199c80c
prasunTranserve <prasun.kumar@transerve.com>
2023-01-18 10:17:58 +0530

    Bank Guarantee Search Criteria file checkin

---
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/models/bankguarantee/BankGuaranteeSearchCriteria.java
@@ -0,0 +1,45 @@
+package org.egov.tl.web.models.bankguarantee;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankGuaranteeSearchCriteria {
+
+	@JsonProperty("applicationNumber")
+	private List<String> applicationNumber;
+
+	@JsonProperty("loiNumber")
+	private String loiNumber;
+
+	@JsonProperty("typeOfBg")
+	private String typeOfBg;
+
+	@JsonProperty("tenantId")
+	private String tenantId;
+
+	@JsonProperty("status")
+	private List<String> status;
+
+	@JsonProperty("ids")
+	private List<String> ids;
+
+	@JsonProperty("fromDate")
+	private Long fromDate = null;
+
+	@JsonProperty("toDate")
+	private Long toDate = null;
+
+	@JsonProperty("offset")
+	private Integer offset;
+
+	@JsonProperty("limit")
+	private Integer limit;
+}
