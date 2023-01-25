b942a6da810f80e28eee43c4d87c5b1bf9220ecd
palamgarg <palamgarg@gmail.com>
2023-01-25 11:08:44 +0530

    Cgnag

---
+++ municipal-services/tl-calculator/src/main/java/org/egov/tlcalculator/web/models/Potential.java
@@ -0,0 +1,13 @@
+package org.egov.tlcalculator.web.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Potential {
+
+	private String name;
+	private String code;
+
+}
