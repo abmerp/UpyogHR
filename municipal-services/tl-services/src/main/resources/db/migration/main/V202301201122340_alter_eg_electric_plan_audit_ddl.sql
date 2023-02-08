8143f20e01e37599cea34f8a681702a2937298da
Digambar2021 <digambar.rajgire@transerve.com>
2023-02-08 19:05:52 +0530

    Add DB Script

--- municipal-services/tl-services/src/main/resources/db/migration/main/V202301201122340_alter_eg_electric_plan_audit_ddl.sql
+++ municipal-services/tl-services/src/main/resources/db/migration/main/V202301201122340_alter_eg_electric_plan_audit_ddl.sql
@@ -3,3 +3,11 @@ ALTER TABLE public.eg_electric_plan_audit ALTER COLUMN id TYPE varchar(255) ;
 ALTER TABLE public.eg_electric_plan_audit ALTER COLUMN id DROP DEFAULT ;
 
 ALTER TABLE public.eg_electric_plan_audit  ADD additionaldetails jsonb NULL;
+
+ALTER TABLE public.eg_electric_plan_audit ADD devName varchar(255) NULL;
+
+ALTER TABLE public.eg_electric_plan_audit ADD developmentPlan varchar(255) NULL;
+
+ALTER TABLE public.eg_electric_plan_audit ADD purpose varchar(255) NULL;
+
+ALTER TABLE public.eg_electric_plan_audit ADD totalArea varchar(255) NULL;
--- municipal-services/tl-services/src/main/resources/db/migration/main/V202301301329342_alter_eg_service_plan_audit_ddl.sql
+++ municipal-services/tl-services/src/main/resources/db/migration/main/V202301301329342_alter_eg_service_plan_audit_ddl.sql
@@ -4,3 +4,11 @@ ALTER TABLE public.eg_service_plan_audit  ADD additionaldetails jsonb NULL;
 ALTER TABLE public.eg_service_plan_audit ALTER COLUMN self_certified_drawings_from_chareted_eng DROP NOT NULL;
 
 ALTER TABLE public.eg_service_plan_audit ALTER COLUMN undertaking DROP NOT NULL;
+
+ALTER TABLE public.eg_service_plan_audit ADD devName varchar(255) NULL;
+
+ALTER TABLE public.eg_service_plan_audit ADD developmentPlan varchar(255) NULL;
+
+ALTER TABLE public.eg_service_plan_audit ADD purpose varchar(255) NULL;
+
+ALTER TABLE public.eg_service_plan_audit ADD totalArea varchar(255) NULL;
